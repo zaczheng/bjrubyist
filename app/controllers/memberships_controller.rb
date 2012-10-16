0ca02443c65881825c1f85d6035c0310d869332e
sundevilyang <kevin.wenyang@gmail.com>
2012-10-17 07:10:19 +0800

    Update the join&quit flass notice in memberships_controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,18 +1,18 @@
 class MembershipsController < ApplicationController
-   before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
 
   def create
     @event = Event.find(params[:membership][:event_id])
     @event.memberships.create!(member_id: current_user.id)
-    flash[:notice] = 'You joined the Event successfully'
+    flash[:notice] = 'You have joined the Event successfully!'
     redirect_to event_path(@event)
   end
 
   def destroy
     @event = Membership.find(params[:id]).event
     @event.memberships.find_by_member_id(current_user.id).destroy
-    flash[:notice] = 'You have quited the Event successfully'
+    flash[:notice] = 'You have quited the Event successfully!'
     redirect_to event_path(@event)
   end
 end
