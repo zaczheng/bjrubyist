fa391bef33fab77615865ee311a99e39f32d0654
sundevilyang <kevin.wenyang@gmail.com>
2012-10-16 11:09:37 +0800

    add people projects join-us path into routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Bjrubyist::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
   resources :users
+
+  match "/people"   => "home#people", as: :people
+  match "/projects" => "home#projects", as: :projects
+  match "/join-us"  => "home#join_us", as: :join_us
 end
