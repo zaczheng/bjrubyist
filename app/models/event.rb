c90c592b415a2afe4443ff659994d11589019b9d
sundevilyang <kevin.wenyang@gmail.com>
2012-10-16 14:47:51 +0800

    Build many-to-many relationship
    members-to-events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :description, :location, :name
+
+  # define membership many-to-many relation
+  has_many :memberships, :dependent => :destroy
+  has_many :members, :through => :memberships
+
+  # def self.find
+  # 	find(params[:id])
+  # end
+
+	def joined?(member)
+		return false if member.nil?
+	  memberships.find_by_member_id(member.id)
+	end
+
 end
