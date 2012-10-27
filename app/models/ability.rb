3cbf54533924e818bd82cbaacdf4110947f59bc1
sundevilyang <kevin.wenyang@gmail.com>
2012-10-27 15:34:14 +0800

    update cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    alias_action :update, :destroy, :to => :modify
+    # can :modify, Comment
     if user.has_role? :admin
       can :manage, :all
+    else
+      can :read, :all
     end
     # Define abilities for the passed in user here. For example:
     #
